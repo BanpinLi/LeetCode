8d7bd9e5fe405242b78a3418e28e090a8fabed1a
BangpingLi <Pin.Lin@foxmail.com>
2021-04-27 23:26:14 +0800

    21.4.27 538-medium commit

---
+++ DFS/538-medium/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    private int sum;
+
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+
+        dfs(root.right);
+        sum += root.val;
+        root.val = sum;
+        dfs(root.left);
+    }
+}
