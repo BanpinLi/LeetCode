4491cb3fd54a613c2efbe04ab37ef1e0d1a6a993
BangpingLi <Pin.Lin@foxmail.com>
2021-09-23 23:45:57 +0800

    21.9.23 326-easy commit

---
+++ Math/326-easy/Solution.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        while (n > 1 && n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
